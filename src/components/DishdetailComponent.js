ccc76c69849d07da1e06341c53e14f09caa04f52
Mohd Aqib <mohd.aqib9811@gmail.com>
2021-01-28 16:45:52 +0530

    Added Header and Footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 function RenderDish({ dish }) {
     return (
         <div className='col-12 col-md-5 m-1'>
-            <Card>
+            <Card >
                 <CardImg width='100%' src={dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
@@ -49,9 +49,11 @@ const DishDetail = (props) => {
         return (<div></div>);
     }
     return (
-        <div className='row'>
-            <RenderDish dish={props.dish} />
-            <RenderComments comments={props.dish.comments} />
+        <div className='container'>
+            <div className='row'>
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.dish.comments} />
+            </div>
         </div>
     );
 }
