ba373543c0712c58fe985c57adb1919c73a22d80
Mohd Aqib <mohd.aqib9811@gmail.com>
2021-02-27 16:25:40 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
 
 function RenderDish({ dish }) {
     return (
@@ -50,10 +50,26 @@ function RenderComments({comments, addComment, dishId}) {
 
 const DishDetail = (props) => {
     const dish = props.dish;
-
-    if (dish == null) {
-        return (<div></div>);
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
     }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
+    {
     return (
         <div className="container">
             <div className="row">
@@ -72,6 +88,7 @@ const DishDetail = (props) => {
             </div>
         </div>
     );
+    }
 }
 
 const required = (val) => val && val.length;
